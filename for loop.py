48616d571719dea0efc21161b8d11e7ab580bb7d
github <tiberiu.i.popescu@gmail.com>
2023-11-03 10:58:47 +0000

    commit2

---
+++ for loop.py
@@ -0,0 +1,2 @@
+for each_item in range(5):
+    print(each_item)
--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 def print_hi(name):
     print(f'Hi, dear {name}')
     if __name__ == '__Main__':
-        print_hi("Pycharm")
-
+        print()
